d6e7e89f51aa45b04ba876e2173b9521b4593a9c
Jianfei PAN <jianfei.pan.tb@gmail.com>
2021-06-03 14:46:12 +0200

    Create ValidBST.cpp

---
+++ ValidBST.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class ValidBST {
+    
+private:
+    TreeNode* mostLeft(TreeNode* root)
+    {
+        if(!root->left)
+            return root;
+        else
+            return mostLeft(root->left);  
+    }
+    
+    TreeNode* mostRight(TreeNode* root)
+    {
+        if(!root->right)
+            return root;
+        else
+            return mostRight(root->right);  
+    }
+    
+public:
+    //from current  to children to find limits 
+    bool isValidBST_1(TreeNode* root) 
+    {
+        bool isValide = false;
+        if(root)
+        {
+            isValide = true;
+        
+            if(root->left)
+            {
+                TreeNode* mostRightNode = mostRight(root->left);
+                isValide = isValide && ((mostRightNode->val)<root->val) && isValidBST(root->left);
+                
+            }
+
+            if(root->right)
+            {
+                TreeNode* mostLeftNode = mostLeft(root->right);
+                isValide = isValide && ((mostLeftNode->val)>root->val) && isValidBST(root->right);
+            }
+        }
+        return isValide;
+    }
+    
+    //faster solution: from current to parent to find limits
+    bool isValidBST(TreeNode* root) {
+        return helper(root, nullptr, nullptr); 
+    }
+    bool helper(TreeNode* root, TreeNode* lefthigh, TreeNode* rightlow){
+        if(root == nullptr){
+            return true;
+        }
+        if(lefthigh != nullptr && root->val <= lefthigh->val){
+            return false;
+        }
+        if(rightlow != nullptr && root->val >= rightlow->val){
+            return false;
+        }
+        return helper(root->right, root, rightlow) && helper(root->left, lefthigh, root);
+    }
+ 
+
+};
