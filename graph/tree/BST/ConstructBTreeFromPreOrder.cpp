239609bafe5e99deba8774c2c380751d09d374a5
Jianfei PAN <jianfei.pan.tb@gmail.com>
2021-11-03 23:24:01 +0100

    Update ConstructBTreeFromPreOrder.cpp
    recusive solution

--- graph/tree/BST/ConstructBTreeFromPreOrder.cpp
+++ graph/tree/BST/ConstructBTreeFromPreOrder.cpp
@@ -57,3 +57,54 @@ TreeNode * constructTree(const vector<string> & nodes)
         
         return root;
     }
+
+//recusive solution
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    size_t findMaxIndexSmallerThan(const vector<int>& preorder, int rootVal, size_t from, size_t to)
+    {
+        size_t index = from;
+        
+        for(; index< preorder.size(); ++index)
+        {
+            if(preorder.at(index)>rootVal)
+            {
+                break;
+            }
+        }
+        //cout<<"index : "<<index-1<<endl;
+        return index-1;
+    }
+    
+    TreeNode* bstFromPreorder(const vector<int>& preorder, int from,  int to)
+    {   TreeNode* root = nullptr;    
+        if(from<preorder.size() && to >= from)
+        {
+            //cout<<from << "to "<<to<<endl;
+            root = new TreeNode(preorder[from]);
+            if(from < to)
+            {
+                size_t leftTreeTo = findMaxIndexSmallerThan(preorder, root->val, from+1, to);
+                root->left = bstFromPreorder(preorder, from+1, leftTreeTo);
+                root->right = bstFromPreorder(preorder, leftTreeTo+1, to);
+            }
+        }
+        return root;
+    }
+public:
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        return bstFromPreorder(preorder, 0, preorder.size()-1);
+    }
+};
