07f9ca7e78e4e9651c1f9856263255cd8e45746c
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-07 15:05:21 +0100

    Update Merge sort.cpp
    add decending pair problem

--- sort/Merge sort.cpp
+++ sort/Merge sort.cpp
@@ -1,14 +1,23 @@
 #include "pch.h"
 #include <iostream>
+#include <utility>
 #include <vector>
 
+
+static void print(const std::vector<std::pair<int, int>> & v)
+{
+   std::cout << "[ ";
+   for (const auto & t : v)
+      std::cout << " [" <<t.first <<","<<t.second << "], ";
+   std::cout << "]" << std::endl;;
+}
 
 template<typename T>
 static void print(const std::vector<T> & v)
 {
    std::cout << "[ ";
-   for (const T & t : v)
-      std::cout <<t << ' ';
+   for (const auto & t : v)
+      std::cout << t << ' ';
    std::cout << "]" << std::endl;;
 }
 
@@ -20,7 +29,7 @@ static void merge(std::vector<int> & s, int from, int mid, int to)
    int right = mid + 1;
    int mergedIndex = 0;
 
-   while (left <= mid  && right <= to)
+   while (left <= mid && right <= to)
    {
       if (s[left] <= s[right])
       {
@@ -80,7 +89,7 @@ merge sort can be a solution for another problem : smaller sum :
 
 given a vector, every elemnt 's  left has its elements smaller than this element, how to summ them all ?
 
-example : 
+example :
 {2,5,3,1,4}
 2--> left smaler : null
 5--> left smaller : 2
@@ -112,7 +121,7 @@ static int mergeSmallerSum(std::vector<int> & s, int from, int mid, int to)
          merged[mergedIndex] = s[left];
          //small sum is produced when we merge
          //left smaller than right, so this left element should be sumed N times where N is the number of element bigger than left, which is to-right+1
-         sum += (to-right+1) * s[left];
+         sum += (to - right + 1) * s[left];
          ++left;
       }
       else
@@ -154,7 +163,7 @@ static int processSmallerSum(std::vector<int> & nums, int from, int to)
 
    const int mid = (from + to) / 2; //>>1
    int leftSum = processSmallerSum(nums, from, mid);
-   int rightSum = processSmallerSum(nums, mid+1, to);
+   int rightSum = processSmallerSum(nums, mid + 1, to);
    int mergSum = mergeSmallerSum(nums, from, mid, to);
 
    return leftSum + rightSum + mergSum;
@@ -162,23 +171,115 @@ static int processSmallerSum(std::vector<int> & nums, int from, int to)
 
 int smallerSum(std::vector<int> & nums)
 {
-   return processSmallerSum(nums, 0 , nums.size()-1);
+   return processSmallerSum(nums, 0, nums.size() - 1);
 }
 
 
+/*
+merge sort can be a solution for another problem : decending pair
+
+given a vector, find all pairs decending from left to right
+
+example :
+{2,5,3,1,4}
+2--> decending pair : [2,1]
+5--> [5,3][5,1][5,4]
+3--> [3,1]
+1--> null
+4--> null
+we can use merge
+
+*/
+static void mergeForDecendingPair(std::vector<int> & s, int from, int mid, int to, std::vector<std::pair<int, int>> & decendingPairs)
+{
+
+   std::vector<int> merged(to - from + 1, 0);
+   int left = from;
+   int right = mid + 1;
+   int mergedIndex = 0;
+
+   while (left <= mid && right <= to)
+   {
+      if (s[left] <= s[right])//equals to be here to make bigger left count the right one: example : merge([2,5], [2,3]) --> 2==2 we need to left++ to make the 5 compare to right 2
+      {
+         merged[mergedIndex] = s[left];
+         ++left;
+      }
+      else
+      {
+         //s[left]> s[right] : decending pair, since left is sorted, all elements after left is bigger than s[right]
+         for (int i = left; i <= mid; ++i)
+         {
+            decendingPairs.emplace_back(s[i], s[right]);
+         }
+         merged[mergedIndex] = s[right];
+         right++;
+
+      }
+      ++mergedIndex;
+   }
+   //rest of left or right
+   while (left <= mid)
+   {
+      merged[mergedIndex] = s[left];
+      ++left;
+      ++mergedIndex;
+   }
+
+   while (right <= to)
+   {
+      merged[mergedIndex] = s[right];
+      ++right;
+      ++mergedIndex;
+   }
+
+   //put merged into original
+   for (int i = 0; i < merged.size(); ++i)
+   {
+      s[i + from] = merged[i];
+   }
+
+}
+
+static void processDecendingPair(std::vector<int> & nums, int from, int to, std::vector<std::pair<int, int>> & decendingPairs)
+{
+   if (from >= to)
+      return;
+
+   const int mid = (from + to) / 2; //>>1
+  processDecendingPair(nums, from, mid, decendingPairs);
+  processDecendingPair(nums, mid + 1, to, decendingPairs);
+  mergeForDecendingPair(nums, from, mid, to, decendingPairs);
+}
+
+std::vector<std::pair<int, int>> decendingPair(std::vector<int> & nums)
+{
+   std::vector<std::pair<int, int>> decendingPairs;
+   processDecendingPair(nums, 0, nums.size() - 1, decendingPairs);
+   return decendingPairs;
+}
 
 int main()
 {
-   std::vector<int> nums = {2,5,3,1,4};
-   
+   std::vector<int> nums = { 2,5,3,1,4 };
+
    print(nums);
 
    std::cout << "sort : \n";
 
-   std::cout << smallerSum(nums) << std::endl;
+   //std::cout << smallerSum(nums) << std::endl;
+   std::vector<std::pair<int, int>> decendingPairs = decendingPair(nums);
+
 
    print(nums);
+
+   print(decendingPairs);
+
+   int i = 1;
+   std::cout << i++;
+
 
    return 0;
 
 }
+
