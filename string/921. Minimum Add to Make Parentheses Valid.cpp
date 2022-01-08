bf8a20110fd42f2b57fe39057ca60a0f0e0e4015
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-08 14:53:52 +0100

    Create 921. Minimum Add to Make Parentheses Valid.cpp

---
+++ string/921. Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  /*
+  A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+
+"()"
+"())"
+"((("
+  */
+public:
+    int minAddToMakeValid(string s) {
+        int minAdd = 0;
+        stack<char> opens;
+        
+        for(char c : s)
+        {
+            if(c==')')
+                if(opens.empty()) ++minAdd;
+                else opens.pop();
+            else 
+                opens.push(c);
+        }
+        
+        while(!opens.empty())
+        {
+            ++minAdd;
+            opens.pop();
+        }
+        
+        return minAdd;
+    }
+};
