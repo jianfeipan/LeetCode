6e84cbc8c108b4303a41b59880f34589a6f6d616
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-08 14:04:18 +0100

    Update 210. Course Schedule typology sort.cpp

--- graph/210. Course Schedule typology sort.cpp
+++ graph/210. Course Schedule typology sort.cpp
@@ -36,11 +36,13 @@ private :
     {
         visited[current] = true;
         for(int dep : graph[current])
-        {
-            if(visited[dep])
-                return true; 
-            if(!courseIsInSequence[current])
+        { 
+            if(!courseIsInSequence[dep])//taken courses can be considered as we don't have this dependency anymore!!!!!!
             {
+                if(visited[dep])
+                //if still get to on node in the path, there is a cycle, the case 1->2->3, 1->0->3, while 3 will not be a cycle becasue it was taken and filtered by the if above
+                    return true;
+                
                 if(DFSHasCycle(dep, graph, visited, courseIsInSequence, sequence))
                 {
                     return true;
