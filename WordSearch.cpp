c2b15adc3342ec7f825f52eccdfe4713946a0800
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-11 22:14:57 +0100

    Update WordSearch.cpp
    with perf enhencement : back track instead of copy the visited flags

--- WordSearch.cpp
+++ WordSearch.cpp
@@ -1,107 +1,75 @@
-class WordSearch {
+class Solution {
+    /*
     
     
-private:
-    bool existOn(const char & target, vector<vector<char>>& board, const string & word, int index, int line, int column)
-    {
-        const char & c = board[line][column];
-
-        if(c==target )
-        {
-            vector<vector<char>> pathMap = board;
-            pathMap[line][column] = '*';
-            if(exist(pathMap, word, index+1, line, column))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
     
-    bool exist(vector<vector<char>>& board, const string &  word, int index, int fromLine, int fromColumn) 
-    {
-        const char & target = word[index];
-        if(index == word.size())
-            return true;
-        
-        //search around
-        if(fromLine>0)
-        {
-            int line = fromLine-1;
-            int column = fromColumn;
-            
-            if(existOn(target, board, word, index, line, column))
-            {
-                return true;
-            }
-        }
-        if(fromColumn>0)
-        {
-            int line = fromLine;
-            int column = fromColumn-1;
-            
-           if(existOn(target, board, word, index, line, column))
-            {
-                return true;
-            }
-        }
-        if(fromLine<board.size()-1)
-        {
-            int line = fromLine+1;
-            int column = fromColumn;
-            
-            if(existOn(target, board, word, index, line, column))
-            {
-                return true;
-            }
-        }
-        if(fromColumn<board[fromLine].size()-1)
-        {
-            int line = fromLine;
-            int column = fromColumn+1;
-            
-            if(existOn(target, board, word, index, line, column))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+    [["A","B","C","E"],
+     ["S","F","E","S"],
+     ["A","D","E","E"]]
+    
+    "ABCESEEEFS"
+    
+    
+    */
+    
+    
+    
+    
     
 public:
     bool exist(vector<vector<char>>& board, string word) 
-    {   
-        //letter are there
-        string wordCopy = word;
-        for(int line = 0; line < board.size(); line++)
-        {
-            for(int column = 0; column < board[line].size(); column++)
-            {
-                std::size_t found = wordCopy.find(board[line][column]);
-                if (found!=std::string::npos)
-                    wordCopy.erase(found,1);
-            }
-        }
+    {
+        //start from very point, if first letter matched, DFS search neighbors
+        if(board.empty() || board[0].empty() || word.empty()) return false;// could be throw exception
         
-        if(!wordCopy.empty())
-            return false;
-        
-        for(int line = 0; line < board.size(); line++)
+        for(size_t lin = 0; lin<board.size(); ++lin)
         {
-            for(int column = 0; column < board[line].size(); column++)
+            for(size_t col = 0; col < board[lin].size(); ++col)
             {
-                if(word[0] == board[line][column])
+                //The same letter cell may not be used more than once.
+                if(board[lin][col] == word[0])
                 {
-                    vector<vector<char>> pathMap = board;
-                    pathMap[line][column] = '*';
-                    if(exist(pathMap, word, 1, line, column))
+                    vector<vector<bool>> used(board.size(), vector<bool>(board[lin].size(), false));
+
+                    if(exist_from(lin, col, board, 0, word, used))
                     {
                         return true;
                     }
                 }
             }
         }
-        
         return false;
     }
+    
+private:
+        bool exist_from(size_t lin, size_t col, const vector<vector<char>>& board, size_t charIndex, const string & word, vector<vector<bool>> & used)
+            //Used need to be passed as a copy, because different branch will have different pathes ---> this will cause a performence problem 
+            // a better solution is track back in the recusive
+        {
+            if(lin<board.size() && col<board[lin].size()
+                && !used[lin][col]
+                && board[lin][col] == word[charIndex])
+            {
+                //cout<<lin <<", "<<col<< " : "<<word[charIndex]<<" ? "<<board[lin][col]<<endl;
+                
+                used[lin][col] = true; 
+                if(charIndex == word.size()-1) return true;
+                
+                if( exist_from(lin-1, col, board, charIndex+1, word, used) 
+                    || exist_from(lin+1, col, board, charIndex+1, word, used) 
+                    || exist_from(lin, col-1, board, charIndex+1, word, used) 
+                    || exist_from(lin, col+1, board, charIndex+1, word, used) )
+                {
+                    return true;
+                }
+                else
+                {
+                    used[lin][col] = false;//------> back track when failed, this branch will stop and it should reput current number aviliable for higher branch 
+                    return false;
+                }
+                
+
+            }
+            return false;
+        }
 };
