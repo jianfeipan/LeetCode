391dbdd307dfbe121a116fffdb3210dededb80de
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-12 19:55:01 +0100

    better clean solution with min max comp

--- table/leet 986. Interval List Intersections.cpp
+++ table/leet 986. Interval List Intersections.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
+    
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) 
+    {
+        return intervalIntersection_simplified(firstList, secondList);
+    }
+
+    vector<vector<int>> intervalIntersection_simplified(vector<vector<int>>& firstList, vector<vector<int>>& secondList) 
+    {
+        vector<vector<int>> result;
+                
+        size_t i=0;
+        size_t j = 0;
+        while(i<firstList.size() && j<secondList.size())
+        {
+            int left = max(firstList[i][0], secondList[j][0]); 
+            int right = min(firstList[i][1], secondList[j][1]); 
+            
+            if(left<=right) result.push_back({left, right});
+            
+            if(firstList[i][1]>=secondList[j][1]) ++j;
+            else ++i;
+        }
+        
+        return result;
+    }
+        
+    
+    vector<vector<int>> intervalIntersection_compares(vector<vector<int>>& firstList, vector<vector<int>>& secondList) 
     {
         
         vector<vector<int>> result;
