a7ba38c98f6078bbe54f1bd84b0bc22814326cc4
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-13 20:34:02 +0100

    Update leet 2006. Count Number of Pairs With Absolute Difference K.cpp

--- table/leet 2006. Count Number of Pairs With Absolute Difference K.cpp
+++ table/leet 2006. Count Number of Pairs With Absolute Difference K.cpp
@@ -1,12 +1,51 @@
 class Solution {
+    
+    /*
+    Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+The value of |x| is defined as:
+
+x if x >= 0.
+-x if x < 0.
+ 
+
+Example 1:
+
+Input: nums = [1,2,2,1], k = 1
+Output: 4
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+1 <= k <= 99
+    */
 public:
     int countKDifference(vector<int>& nums, int k) 
     {
-        return countKDifference_map_cout_two_pointer(nums, k);
+        return countKDifference_vector_cout(nums, k);
     }
 private:
     
-    int countKDifference_map_cout_two_pointer(vector<int>& nums, int k) 
+     int countKDifference_vector_cout(vector<int>& nums, int k) //O(N) -->very smart solution
+    {
+        if(nums.empty()) return -1;//thow exception
+     
+        int count = 0;
+        
+         //1 <= nums[i] <= 100!!!!!!! bitmap instead of map
+        int diagram[101] = {};
+        for(int num : nums) ++diagram[num]; 
+        
+        for(int i = 1; i <= 100; ++i)
+        {
+            if(i - k>=0 && i-k<=100)
+                count += diagram[i] * diagram[i - k];
+        }
+        
+        return count;
+    }
+    
+    int countKDifference_map_cout_two_pointer(vector<int>& nums, int k) //O(N)
     {
         if(nums.empty()) return -1;//thow exception
      
