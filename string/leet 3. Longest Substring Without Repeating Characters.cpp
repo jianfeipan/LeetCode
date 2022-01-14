515b3432dbc78dac7684b0d90a0125ab89c79c83
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-14 11:02:23 +0100

    Update leet 3. Longest Substring Without Repeating Characters.cpp

--- string/leet 3. Longest Substring Without Repeating Characters.cpp
+++ string/leet 3. Longest Substring Without Repeating Characters.cpp
@@ -4,21 +4,22 @@ public:
     {
         int maxLen = 0;
         
-        unordered_set<char> current;//can be replaced by bitmap bool[256]
+        //unordered_set<char> current;
+        int current[256] = {0};//use bitmap
         int left=0;
         int right = 0;
         
         while(right< s.size())
         {
-            while(current.count(s[right]))
+            while(current[s[right]] )//right letter is in current map--> i need to move left to remove this duplication
             {
-                current.erase(s[left]);
+                current[s[left]]=0;
                 ++left;
             }
             
-            current.insert(s[right]);
+            current[s[right]] = 1;// no more duplication on right, mark right in current, move to next
             right++;
-            if(right - left +1 > maxLen)
+            if(right - left +1 > maxLen)//every time right increase one, we have the possiblility to have a max length 
             {
                 maxLen = right - left;
             }
