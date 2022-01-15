ca528baa8832c4c978281891468a5a5bd1199814
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-15 11:51:18 +0100

    Update 146. LRU Cache.cpp

--- struct_design/146. LRU Cache.cpp
+++ struct_design/146. LRU Cache.cpp
@@ -30,61 +30,68 @@ lRUCache.get(4);    // return 4
   
   */
 public:
-    LRUCache(int capacity) {
-        _capacity = capacity;
+    LRUCache(int capacity):
+    _capacity(capacity),
+    _data(),
+    _mostRencentUsedKey()
+    {
     }
     
-    int get(int key) {
-        auto it = _cache.find(key);
-        
-        if(it!=_cache.end())
+    int get(int key) 
+    {
+        if(_data.count(key))
         {
-            
             markAsUsed(key);
-            return it->second.first;
+            return _data[key].first;
+        }
+        else return -1;
+    }
+    
+    void put(int key, int value) 
+    {
+        if(_data.count(key))
+        {
+            _data[key].first = value;
+            markAsUsed(key);
         }
         else
         {
-            return -1;
-        }
-    }
-    
-    void put(int key, int value) {
-        auto it = _cache.find(key);
-        if(it == _cache.end())
-        {
-            _keys.push_front(key);
-            if(_capacity>0)
-            {
-                --_capacity;
-            }
-            else
-            {
-                _cache.erase(_keys.back());
-                _keys.pop_back();
-            }
-        }
-        else
-        {
+            --_capacity;
+            if(_capacity==0)
+                eraseLeastUsed();
             
-            markAsUsed(key);
+            _mostRencentUsedKey.push_front(key);
+            _data[key] = {value, _mostRencentUsedKey.begin()};
         }
-        _cache[key] = { value, _keys.begin() };
-        
     }
     
 private:
+    
+    void eraseLeastUsed()
+    {
+        auto leastUsedKey = _mostRencentUsedKey.back();
+        _mostRencentUsedKey.pop_back();
+        _data.erase(leastUsedKey);  
+        ++_capacity;
+    }
+    
     void markAsUsed(int key)
     {
-        _keys.erase(_cache[key].second);//make use of map
-        _keys.push_front(key);
-        _cache[key].second = _keys.begin();
+        if(_data.count(key))
+        {
+            auto it = _data[key].second;
+            
+            _mostRencentUsedKey.erase(it);
+            _mostRencentUsedKey.push_front(key);
+            _data[key].second = _mostRencentUsedKey.begin();
+        }
     }
     
+    
 private:
-    unordered_map<int, pair<int, list<int>::iterator> > _cache; //fonction 1 : cache to access value 
-    list<int> _keys; //function 2 : limited capacity to remove old least recent used key
     int _capacity;
+    unordered_map<int, pair<int, list<int>::iterator>> _data;
+    list<int> _mostRencentUsedKey;
 };
 
 /**
