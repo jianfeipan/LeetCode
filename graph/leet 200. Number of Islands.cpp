2a98c9230f7d49f36bf556a803956b270ab7edb0
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-15 11:17:05 +0100

    Update leet 200. Number of Islands.cpp
    with better directions management

--- graph/leet 200. Number of Islands.cpp
+++ graph/leet 200. Number of Islands.cpp
@@ -1,51 +1,60 @@
-  class Solution {
+class Solution {
+    /*
+    
+    4 or 8 direction adjacent ? 4    
+    
+    */
+    
+    
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        //not good thing to modify input, except allowed
-        vector<vector<char>>& map = grid;
+    
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        return numIslands_DFS(grid);
+    }
+    
+private:
+    int numIslands_DFS(vector<vector<char>> grid) 
+    {    
+        int islandCount = 0;
         
-        int count = 0;
-        for(int i = 0; i<map.size(); ++i)
+        for(size_t lin = 0; lin<grid.size(); ++lin)
         {
-            for(int j = 0; j<map[i].size(); ++j)
+            for(size_t col = 0; col<grid[lin].size(); ++col)
             {
-                if(map[i][j] == '1')
+                if(grid[lin][col] == '1')
                 {
-                    ++count;
-                    DFSVisit(map, i, j);
+                    ++islandCount;  
+                    visit(lin, col, grid);
                 }
             }
-
         }
-        return count;
-        
+        return islandCount;
     }
-    void DFSVisit(vector<vector<char>>& map, int i, int j)
-    {
-        if(map[i][j] =='1')
-        {
-            map[i][j] = '0';
-            if(i-1>=0)
-            {
-                DFSVisit(map, i-1, j);
-            }
-            if(i+1<map.size())
-            {
-                DFSVisit(map, i+1, j);
-
-            }
-            if(j-1>=0)
-            {
-                DFSVisit(map, i, j-1);
-
-            }
-            if(j+1<map[i].size())
-            {
-                DFSVisit(map, i, j+1);
-
-            }
-        }
-    }
-
     
+    void visit(int lin, int col, vector<vector<char>> & grid)
+    {
+        if(lin >=0 && lin<grid.size() && col>=0 && col<grid[lin].size())
+        {
+            if(grid[lin][col] == '1')
+            {
+                grid[lin][col] = 'X';//mark as visited
+                const vector<pair<int, int>> & moves = moveInDirections();
+                
+                for(const auto & move : moves)
+                    visit(lin + move.first, col + move.second , grid);
+            }
+        }
+    }
+    
+    const static vector<pair<int, int>> moveInDirections()
+    {
+        static vector<pair<int, int>> directions({
+            {-1, 0},
+            { 1, 0},
+            { 0,-1},
+            { 0, 1}
+        });
+        return directions;
+    }
 };
