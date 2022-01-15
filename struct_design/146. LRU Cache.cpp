4407c854800fd454a9fabc77a001c7d3beb2d234
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-15 11:54:03 +0100

    Update 146. LRU Cache.cpp

--- struct_design/146. LRU Cache.cpp
+++ struct_design/146. LRU Cache.cpp
@@ -1,34 +1,4 @@
 class LRUCache {
-  
-  /*
-  Input
-["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
-[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
-Output
-[null, null, null, 1, null, -1, null, -1, 3, 4]
-
-Explanation
-LRUCache lRUCache = new LRUCache(2);
-lRUCache.put(1, 1); // cache is {1=1}
-lRUCache.put(2, 2); // cache is {1=1, 2=2}
-lRUCache.get(1);    // return 1
-lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-lRUCache.get(2);    // returns -1 (not found)
-lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-lRUCache.get(1);    // return -1 (not found)
-lRUCache.get(3);    // return 3
-lRUCache.get(4);    // return 4
-
-  
-["LRUCache","put","put","get","put","get"]
-[[2],[2,2],[1,1],[1],[3,3], [2]]
-["LRUCache","put","put","get","put","get","put","get","get","get"]
-[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]
-["LRUCache","put","put","get","put","put","get"]
-[[2],[2,1],[2,2],[2],[1,1],[4,1],[2]]
-  
-  
-  */
 public:
     LRUCache(int capacity):
     _capacity(capacity),
@@ -56,10 +26,11 @@ public:
         }
         else
         {
-            --_capacity;
-            if(_capacity==0)
+            if(--_capacity<0)
+            {
                 eraseLeastUsed();
-            
+                ++_capacity;
+            }
             _mostRencentUsedKey.push_front(key);
             _data[key] = {value, _mostRencentUsedKey.begin()};
         }
@@ -72,7 +43,6 @@ private:
         auto leastUsedKey = _mostRencentUsedKey.back();
         _mostRencentUsedKey.pop_back();
         _data.erase(leastUsedKey);  
-        ++_capacity;
     }
     
     void markAsUsed(int key)
