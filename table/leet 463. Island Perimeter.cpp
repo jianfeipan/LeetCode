f5759a6f396132621cf275a1a578745a4f8dd09d
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 20:50:50 +0100

    Update leet 463. Island Perimeter.cpp

--- table/leet 463. Island Perimeter.cpp
+++ table/leet 463. Island Perimeter.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
+    int islandPerimeter(vector<vector<int>>& grid) 
+    {
+        return islandPerimeter_one_loop(grid);
+    }
+
+    int islandPerimeter_one_loop(const vector<vector<int>>& grid)
+    {
+        int perimeter = 0;
+        
+        for(int lin = 0; lin < grid.size(); ++lin)
+        {
+            for(int col = 0; col < grid[lin].size(); ++col)
+            {
+                if(grid[lin][col] == 1)
+                {
+                    perimeter += 4;
+                    if(col>=1 && grid[lin][col-1] == 1) 
+                        perimeter-=2;//only count what 's the impact if add one cell on the right
+                    if(lin>=1 && grid[lin-1][col] == 1)
+                        perimeter-=2;
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+    
+    int islandPerimeter_vertical_then_hrizonal(vector<vector<int>>& grid) {
         int perimeter = 0;
         
         if(grid.empty() || grid[0].empty()) throw invalid_argument("empty island.");
