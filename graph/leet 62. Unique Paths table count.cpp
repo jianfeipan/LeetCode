c3ad50c9569e5db39672511869e739340c035396
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 18:35:26 +0100

    Update leet 62. Unique Paths table count.cpp

--- graph/leet 62. Unique Paths table count.cpp
+++ graph/leet 62. Unique Paths table count.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     int uniquePaths(int m, int n) 
     {
-        return uniquePaths_DP(m ,n);
+        return uniquePaths_DP_plus(m ,n);
     }   
     
 private:
-    
+    int uniquePaths_DP_plus(int m, int n) 
+        //if we see the usage of this matrix, every cell only depends really on one cell before
+        //so we can use only one vector instead of the matrix, to to the incremental on it self 
+    {
+        vector<int> pathes(n, 1);
+       for(int lin = m-2; lin>=0; --lin)
+       {
+           for(int col = n-2; col>=0; --col)
+           {
+               pathes[col] += pathes[col+1]; 
+           }
+       }
+       
+       return pathes[0];
+    } 
    int uniquePaths_DP(int m, int n) 
    {
        vector<vector<int>> pathes(m, vector<int>(n, 1));
