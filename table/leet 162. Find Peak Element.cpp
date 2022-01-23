4edc756225c1dc3e88524a9761c09e5559f2c019
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 21:16:38 +0100

    Update leet 162. Find Peak Element.cpp

--- table/leet 162. Find Peak Element.cpp
+++ table/leet 162. Find Peak Element.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int findPeakElement(const vector<int> &num) {
-        return binarySearch(num, 0, num.size()-1);
-    }
-private:
-    int binarySearch(const vector<int> &num, int low, int high)
+    int findPeakElement(vector<int>& nums) 
     {
-        if(low == high)
-            return low;
-        else
-        {
-            int mid = (low+high)/2;
-
-            if(num[mid] < num[mid+1])
-            // pick is that you are looking for a direction, going up or going down, from middle you know go to which direction, then you focus on the go up direction part, because the -1 and  infini will be INT_MIN, sure it's a down.
-                return binarySearch(num, mid+1, high);
-            else
-                return binarySearch(num, low, mid);
-        }
+        //first decending point is a peak
+        return findPeak(nums, 0, nums.size()-1);
     }
+    
+    int findPeak(vector<int>& nums, int from, int to)
+    {
+        if(from == to) return from;
+        
+        int mid = (from+to)>>1;
+        
+        if(nums[mid] < nums[mid+1]) return findPeak(nums, mid+1, to);//on mid point, it rises, so the peak is on the right
+        else                        return findPeak(nums, from, mid);
+        
+    }
+ 
 };
