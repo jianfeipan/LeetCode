4321df9d4586644370dce2def3928f240ce30166
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 13:35:11 +0100

    Create leet 1396. Design Underground System subway avg time.cpp

---
+++ struct_design/leet 1396. Design Underground System subway avg time.cpp
@@ -0,0 +1,46 @@
+class UndergroundSystem {
+public:
+    UndergroundSystem():_sumAndCount(), _checkIn()
+    {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) 
+    {
+        _checkIn[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) 
+    {
+        const int rideTime = t - _checkIn[id].second;
+        auto & sumAndCount  =  _sumAndCount[_checkIn[id].first][stationName];
+        sumAndCount.first+=rideTime;
+        ++sumAndCount.second;
+    }
+    
+    double getAverageTime(string startStation, string endStation) //const
+    {
+        if(!_sumAndCount.count(startStation) ||  !_sumAndCount[startStation].count(endStation)) 
+            throw invalid_argument("no recode for "+startStation+" - "+endStation);
+        
+        const auto & sumAndCount  =  _sumAndCount[startStation][endStation];
+        return (double)sumAndCount.first/sumAndCount.second;
+    }
+    
+private:
+    using TotalTime = int;
+    using RideCount = size_t;
+    using RideMetric = pair<TotalTime, RideCount>;
+    using Destimations = unordered_map<string, RideMetric>;
+    
+    unordered_map<  string, Destimations> _sumAndCount;
+    unordered_map<int, pair<string, int>> _checkIn;
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
