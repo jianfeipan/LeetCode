24b39c492414bf8d127f5dc5a5d2bad8f86076b6
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 17:04:44 +0100

    Update Leet 1249. Minimum Remove to Make Valid Parentheses.cpp

--- string/Leet 1249. Minimum Remove to Make Valid Parentheses.cpp
+++ string/Leet 1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        string result;
         stack<size_t> opens;
+        
         for(size_t i = 0; i<s.size(); ++i)
         {
-            char & c = s[i];
-            
-            if(c=='(')
-                opens.push(i);
-            else if(c==')')
+            if(s[i] == '(') opens.push(i);
+            else if(s[i] == ')')
             {
-                if(opens.empty())
-                {
-                    c = '!';
-                }
-                else
-                {
-                    opens.pop();
-                }
+                if(opens.empty()) s[i] = '*';
+                else opens.pop();
             }
         }
         
-        while(!opens.empty())
-        {
-            s[opens.top()] = '!';
-            opens.pop();
-        }
+        while(!opens.empty()) s[opens.top()] = '*',opens.pop();
         
+        string validStr("");
         
-        //s.erase(remove(s.begin(), s.end(), '*'), s.end());
-        for(char c : s)
-        {
-            if(c!='!')
-                result+=c;
-        }
+        for(auto c : s) if(c != '*') validStr+=c;
         
-        return result;
-        
+        return validStr;
     }
 };
