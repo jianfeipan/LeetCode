2edbcdb177184b51bb1b83b592afbf54b9d4ba86
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-23 22:37:53 +0100

    Update Leet 1029. Two City Scheduling.cpp

--- DP/Leet 1029. Two City Scheduling.cpp
+++ DP/Leet 1029. Two City Scheduling.cpp
@@ -4,23 +4,17 @@ public:
         //idea: put everyone to A first, then see move who will save the most, then we move another....util we have a half
         //half is k person, so we need the k top diff --> maxHeap
         
-        priority_queue<int> maxHeap;//by default it's a max heap
-        int totalCosts = 0;
-        for(const auto & cost : costs)
-        {
-            totalCosts += cost.at(0);
-            maxHeap.push(cost.at(0) - cost.at(1));
-        }
-
-        int bestSave = 0;//by moving people from A to B
+        int costAllCityA = 0;
+        for(const auto & cost : costs) costAllCityA+=cost[0];
         
+        priority_queue<int> changeToBCouldSave;
+        for(const auto & cost : costs) changeToBCouldSave.push(cost[0] - cost[1]);
+        
+        int maxSaveMoveToB = 0;
         for(int i = 0; i<costs.size()/2; ++i)
         {
-            bestSave+=maxHeap.top(); maxHeap.pop();
+            maxSaveMoveToB += changeToBCouldSave.top(),changeToBCouldSave.pop();
         }
-        
-        return totalCosts - bestSave;
-        
-        
+        return costAllCityA - maxSaveMoveToB;
     }
 };
