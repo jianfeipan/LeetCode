663b9ce571686365cbfe2341e2b84b82f2f09e76
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-01-28 19:28:42 +0100

    Update leet 979. Distribute Coins in Binary Tree.cpp

--- graph/tree/leet 979. Distribute Coins in Binary Tree.cpp
+++ graph/tree/leet 979. Distribute Coins in Binary Tree.cpp
@@ -17,81 +17,28 @@ In one move, we may choose two adjacent nodes and move one coin from one node to
 
 Return the minimum number of moves required to make every node have exactly one coin.
 
-failed test: 
-Input
-[0,3,1,null,null,null,1,null,0]
-Output
-7
-Expected
-5
 */
-class Solution 
-{
+class Solution {
 public:
     int distributeCoins(TreeNode* root) 
     {
-        if(root)
-        {
-            if(root->left && root->right)
-            {
-                pair<int, int> leftTree = countTree(root->left);
-                const int leftNeed = leftTree.first - leftTree.second;
-                
-                pair<int, int> rightTree = countTree(root->right);
-                 const int rightNeed = rightTree.first - rightTree.second;
-                
-                
-                const int stepsNeededOnRoot = abs(leftNeed) + abs(1 + leftNeed - root->val);
-                    
-                    // (leftNeed - root->val + 1)/*root need get from right*/
-                     //  leftNeed/*then give left what left needs*/;
-                
-                //update nodes
-                root->left->val+= leftNeed;//left get what he needed;
-                root->right->val+=rightNeed;
-                root->val = 1;
-                
-                const int leftSteps = distributeCoins(root->left);
-                const int rightSteps = distributeCoins(root->right);
-                
-                return leftSteps+ rightSteps + abs(stepsNeededOnRoot);
-            }
-            else if(root->left || root->right)
-            {
-                const int rootNeed = 1 - root->val;
-                
-                
-                //give all rest to left or right since only one child
-                root->val = 1;
-                
-                TreeNode * child = root->left? root->left : root->right;
-                
-                child->val -= rootNeed;
-                
-                const int childSteps = distributeCoins(child);
-                
-                return childSteps + abs(rootNeed);
-            }
-            else
-            {
-                return 0;
-            }
-            
-        }
-        return 0;
+        int need;
+        return distributeCoins(root, need);
     }
     
 private:
-    //count nodes number and coins number
-    pair<int, int> countTree(TreeNode* root)
+    int distributeCoins(TreeNode* root, int & need) 
     {
-        if(root)
-        {
-            pair<int, int> left = countTree(root->left);
-            pair<int, int> right = countTree(root->right);
-            
-            return {left.first+right.first +1, left.second+right.second + root->val};
-        }
-        return {0, 0};
+        if(!root) return 0;
+        
+        int leftNeed = 0;
+        auto leftSteps = distributeCoins(root->left, leftNeed);
+        
+        int rightNeed = 0;
+        auto rightSteps = distributeCoins(root->right, rightNeed);
+        
+        const int rest = root->val - leftNeed - rightNeed;
+        need = 1 - rest;//I need from parent to have 1
+        return abs(need) +rightSteps+leftSteps;
     }
 };
