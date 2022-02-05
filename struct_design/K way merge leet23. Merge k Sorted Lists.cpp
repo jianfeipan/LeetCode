9091c8fa222f8edb06a5c66fa34ca9d73513302a
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-02-05 17:20:22 +0100

    Update and rename sort/K way merge leet23. Merge k Sorted Lists.cpp to struct_design/K way merge leet23. Merge k Sorted Lists.cpp

--- struct_design/K way merge leet23. Merge k Sorted Lists.cpp
+++ struct_design/K way merge leet23. Merge k Sorted Lists.cpp
@@ -10,8 +10,42 @@
  */
 class Solution {
 public:
+    //N way merge using minHeap
+    ListNode* mergeKLists(vector<ListNode*>& lists) //O(N * logK) K lists, N nodes
+    {
+        ListNode merged;
+        
+        ListNode * currentMerged = &merged;
+        
+        auto greater = [](ListNode* left, ListNode* right){
+            return left->val > right->val;
+        };
+        
+        priority_queue<ListNode*,
+            vector<ListNode*>,
+            decltype(greater)
+        > minHeap(greater);
+        
+        for(auto * head : lists) 
+            if(head) minHeap.push(head);
+        
+        while(!minHeap.empty())
+        {
+            
+            auto * minNode = minHeap.top();minHeap.pop();
+            if(auto * next = minNode->next)
+                minHeap.push(next);
+            
+            currentMerged->next = new ListNode(minNode->val);
+            currentMerged = currentMerged->next;
+        }
+        
+        return merged.next;
+    }
+    
+    
     //N way merge
-    ListNode* mergeKLists(vector<ListNode*>& lists) 
+    ListNode* mergeKLists_with_compare_loop(vector<ListNode*>& lists)  //O(N * K) K lists, N nodes
     {
         ListNode merged;
         
