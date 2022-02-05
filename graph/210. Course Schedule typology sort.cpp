7261f5ab2e75d99bcb4bdf887d3f91b361547594
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-02-05 12:08:16 +0100

    Update 210. Course Schedule typology sort.cpp

--- graph/210. Course Schedule typology sort.cpp
+++ graph/210. Course Schedule typology sort.cpp
@@ -1,126 +1,46 @@
 class Solution {
 public:
-    //idea1 is DFS + cycle detection
-    //idea2 is store nodes and the dependency coutns (indegree), if there is a 0, we take it, then we update -> recount, if no 0, there is a cycle
-    
-    
-    
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites)
     {
+        /*
+        Typological sort
+        BFS
         
-        return findOrder_node_indegree(numCourses, prerequisites);
-    }
-    
-    vector<int> findOrder_node_indegree(int numCourses, vector<vector<int>>& prerequisites) 
-    {
-        unordered_map<int, Node> nodes;
-        for (const vector<int> prerequisite: prerequisites) {
-            nodes[prerequisite[1]].postRequests.push_back(prerequisite[0]);
-            nodes[prerequisite[0]].preRequestNb += 1;
-        }
-
-        vector<int> order;
+        */
+        unordered_map<int, unordered_set<int>> nextSteps;
+        vector<int> preRequestCounts(numCourses, 0);
         
-        queue<int> sources;
-        //this queue is to note all the impacted nodes, which are possible nodes with  preRequestNb == 0, and they will impact other nodes
-        
-        //find the course with no dependency : preRequestNb == 0
-        for (int i = 0; i < numCourses; ++i) 
+        for(const auto & prerequisite : prerequisites)
         {
-            if (nodes[i].preRequestNb == 0) 
+            if(nextSteps[prerequisite[1]].insert(prerequisite[0]).second)//avoid duplications
+                ++preRequestCounts[prerequisite[0]];
+        }
+        
+        vector<int> coursesOrder;coursesOrder.reserve(numCourses);
+        queue<int> toDo;
+        
+        for(int course = 0; course < numCourses; ++course)
+        {
+            if(preRequestCounts[course] == 0)
             {
-                order.push_back(i);
-                sources.push(i);
+                toDo.push(course);
             }
         }
-
-        while (!sources.empty()) 
+        
+        while(!toDo.empty())
         {
-            int num_sources = sources.size();
-            for (int i = 0; i < num_sources; ++i) 
+            int currentCourse = toDo.front();toDo.pop();
+            coursesOrder.push_back(currentCourse);
+            
+            for(const auto & nextStep : nextSteps[currentCourse])
             {
-                int source = sources.front();
-                sources.pop();
-                for (int postRequest : nodes[source].postRequests) 
+                if(--preRequestCounts[nextStep] == 0)
                 {
-                    nodes[postRequest].preRequestNb -= 1;
-                    if (nodes[postRequest].preRequestNb == 0) 
-                    {
-                        order.push_back(postRequest);
-                        sources.push(postRequest);
-                    }
+                    toDo.push(nextStep);
                 }
             }
         }
-
-        return order.size() == numCourses? order : vector<int>();
-    }
-
-
-    vector<int> findOrder_DFS(int numCourses, vector<vector<int>>& prerequisites) //O(E+V)
-    {
-        std::map<int, std::set<int>> graph;
-        for(const auto & dep : prerequisites)
-        {
-            graph[dep[0]].insert(dep[1]);
-        }
-        
-        vector<int> sequence;
-        sequence.reserve(numCourses);
-        
-        std::vector<bool> courseIsInSequence(numCourses, false);
-        
-        for(int i = 0; i<numCourses; ++i)
-        {
-            if(!courseIsInSequence[i])
-            {
-                std::vector<bool> visited(numCourses, false);
-                if(DFSHasCycle(i, graph, visited, courseIsInSequence, sequence))
-                {
-                    return {};
-                }
-            }
-        }
-        
-        return sequence;
-    }
-    
-private : 
-    
-    struct Node {
-        int preRequestNb = 0;//I depend on how many nodes
-        vector<int> postRequests;//who depends on me
-    };
-    
-    
-    bool DFSHasCycle(int current, 
-             std::map<int, std::set<int>> & graph, 
-             std::vector<bool> & visited, 
-             std::vector<bool> & courseIsInSequence, 
-             std::vector<int> & sequence)
-    {
-        visited[current] = true;
-        for(int dep : graph[current])
-        { 
-            if(!courseIsInSequence[dep])//taken courses can be considered as we don't have this dependency anymore!!!!!!
-            {
-                if(visited[dep])
-                //if still get to on node in the path, there is a cycle, the case 1->2->3, 1->0->3, while 3 will not be a cycle becasue it was taken and filtered by the if above
-                    return true;
-                
-                if(DFSHasCycle(dep, graph, visited, courseIsInSequence, sequence))
-                {
-                    return true;
-                }
-            }
-        }
-        
-        if(!courseIsInSequence[current])
-        {
-            sequence.push_back(current);
-            courseIsInSequence[current] = true;
-        }
-        
-        return false;
+        if(coursesOrder.size() == numCourses) return coursesOrder;
+        else return {};//cycle case
     }
 };
