4700874112cd96b00dbdeb0db53e51e493e2787f
Jianfei PAN <jianfei.pan.tb@gmail.com>
2022-02-06 18:08:21 +0100

    Create leet 140. Word Break II.cpp

---
+++ DP/leet 140. Word Break II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) 
+    {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        
+        unordered_map<string, vector<string>> memory;
+        
+        return wordBreak(s, dict, memory);
+        
+    }
+    
+    const vector<string> & wordBreak(string s, const unordered_set<string>& dict,
+                             unordered_map<string, vector<string>> & memory) 
+    {
+        auto it = memory.find(s);
+        
+        if(it!=memory.end()) return it->second;
+        
+        vector<string> breaked;
+        if(dict.count(s)) breaked.push_back(s);
+        string current;
+        for(const auto & c : s)
+        {
+            current+=c;
+            if(dict.count(current))
+            {
+                const auto &  nexts = wordBreak(s.substr(current.size()), dict, memory);
+                for(const auto & next : nexts )
+                {
+                    breaked.push_back(current + " " + next);
+                }
+            }
+        }
+        memory[s] = breaked;
+        return memory[s];
+    }
+};
