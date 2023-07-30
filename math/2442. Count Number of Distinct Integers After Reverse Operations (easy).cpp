9e2a7b3a196220a603f731e52973a7799b96b5c7
Jianfei PAN <jianfei.pan.tb@gmail.com>
2023-07-30 14:49:20 +0100

    Create 2442. Count Number of Distinct Integers After Reverse Operations (easy).cpp

---
+++ math/2442. Count Number of Distinct Integers After Reverse Operations (easy).cpp
@@ -0,0 +1,57 @@
+/*
+
+You are given an array nums consisting of positive integers.
+
+You have to take each integer in the array, reverse its digits, and add it to the end of the array. You should apply this operation to the original integers in nums.
+
+Return the number of distinct integers in the final array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,13,10,12,31]
+Output: 6
+Explanation: After including the reverse of each number, the resulting array is [1,13,10,12,31,1,31,1,21,13].
+The reversed integers that were added to the end of the array are underlined. Note that for the integer 10, after reversing it, it becomes 01 which is just 1.
+The number of distinct integers in this array is 6 (The numbers 1, 10, 12, 13, 21, and 31).
+Example 2:
+
+Input: nums = [2,2,2]
+Output: 1
+Explanation: After including the reverse of each number, the resulting array is [2,2,2,2,2,2].
+The number of distinct integers in this array is 1 (The number 2).
+ 
+*/
+
+
+class Solution {
+
+private:
+    int reverse(int original)
+    {
+        int reversed = 0;
+
+        while(original)
+        {
+            reversed*=10;
+            reversed+=original%10;
+            original/=10;
+        }
+
+        return reversed;
+    }
+public:
+    int countDistinctIntegers(vector<int>& nums) 
+    {
+        int originalSize = nums.size();
+        unordered_set<int> distinctNums;
+        for(int i = 0; i<originalSize; ++i)
+        {
+            distinctNums.insert(nums[i]);
+            distinctNums.insert(reverse(nums[i]));
+        }
+
+        return distinctNums.size();
+    }
+};
