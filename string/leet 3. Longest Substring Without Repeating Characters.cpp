01fdd919f88739e8a74187bfa07c0b6ae929c2c9
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-01-30 15:05:35 +0100

    Update leet 3. Longest Substring Without Repeating Characters.cpp

--- string/leet 3. Longest Substring Without Repeating Characters.cpp
+++ string/leet 3. Longest Substring Without Repeating Characters.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.empty()) return 0;
+        if(s.size() == 1) return 1;
+
+        int maxLen = 1;
+        unordered_set<char> chars;
+        chars.insert(s[0]);
+        int i = 0;
+        int j = 1; 
+        while(j<s.size()){
+            const char current = s[j];
+            //duplication
+            if(chars.count(current) == 0){
+                chars.insert(current);
+            } else{
+                maxLen = max(maxLen, j-i);
+                while(s[i] != current){
+                    chars.erase(s[i]);
+                    ++i;
+                }//move to the duplicated position
+                ++i;// goover the duplication
+            }
+            ++j;
+        }
+        if(j==s.size())
+            maxLen = max(maxLen, j-i);
+
+        // 0 1 2 3 4 5 6 7
+        // a b c a b c d d
+        // i.    j
+        // i need to go right remove the letters from set, util no more duplications
+        return maxLen;
+    }
+};
+
+
+
     int lengthOfLongestSubstring(string s) 
     {
         int maxLen = 0;
