f866f3e1ce6e579156e2f78d55539725869ade28
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-02-07 11:46:35 +0100

    Update leet almost sorted 153. Find Minimum in Rotated Sorted Array.cpp

--- table/leet almost sorted 153. Find Minimum in Rotated Sorted Array.cpp
+++ table/leet almost sorted 153. Find Minimum in Rotated Sorted Array.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {
-        size_t left = 0;
-        size_t right = nums.size()-1;
-        
-        while(left < right)
-        {
-            cout<< left <<", "<<right;
-            size_t mid = (left+right)>>1;
-            
-            if(nums[mid] > nums[mid+1]) return nums[mid+1];
-            else 
-                if(nums[left] > nums[mid]) right = mid;
-                else left = mid+1;
+    int findMin(vector<int> &nums) {
+        //logN -> binary search
+        //[3,4,5,6,1,2]
+        // l-----r l-r
+        // binary search find where left > right (other places we have r>l)
+        int l = 0;
+        int r = nums.size()-1;
+        int min=nums[0];
+        while(l<=r){
+            int mid=l+(r-l)/2; // 防止益处
+            if(nums[mid]<min){
+                r=mid-1;
+                min=nums[mid];
+            }else{
+                l=mid+1;
+            }
         }
-        
-        return nums[0];
+        return min;
     }
 };
