5c81a84fb4d0a5ad672d5bc0b4fb8b048cf76603
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-02-10 23:36:23 +0100

    Update 287. Find the Duplicate Number.cpp

--- table/287. Find the Duplicate Number.cpp
+++ table/287. Find the Duplicate Number.cpp
@@ -17,28 +17,106 @@ Input: nums = [3,1,3,4,2]
 Output: 3
 */
 class Solution {
-
-/*
-fact: 
-    N+1 integers in [1, n]  --> at least one duplication
-
-idea:
-    initial soltion: mark them in a vector, (histogram)
-
-O(n)
-
-*/
 public:
-    int findDuplicate(vector<int>& nums) {
-        vector<int> count(nums.size(), 0);
+
+    // BF: use a data struture: unodered_set
+    // time: O(N)
+    // space: O(N)
+    int findDuplicate_set(vector<int>& nums) 
+    {
+        unordered_set<int> unique;
         for(auto num : nums)
         {
-            ++count[num-1];
-            if(count[num-1]>1)
-            {
-                return num;
+            if(unique.contains(num)) return num;
+            unique.insert(num);
+        }
+
+        return -1;
+    }
+
+    // nums containing  n+1 integer, [1...n]
+    int findDuplicate_bitmap(vector<int>& nums)
+    {   
+        vector<bool> presented(nums.size(), false);
+        for(auto num : nums)
+        {
+            if(presented[num-1]) return num;
+            else presented[num-1] = true;
+        }
+        return -1;
+    }
+    // space O(1)?
+    // sorting, then one pass, but time O(NlogN)
+    //
+    // negative marking!!
+    // use the -1 as the flag 
+    // the [1...n] numers can be used as index to nums of this vector
+    // if we got one number already negative, it's the duplicated one
+    int findDuplicate_negative_marked(vector<int>& nums)
+    {   
+        for(int i = 0; i<nums.size(); ++i)
+        {
+            int current = nums[i];
+            if(current<0) current*=-1;
+            if(nums[current] < 0) return current;
+            nums[current]*=-1;
+        }
+        
+        return -1;
+    }
+
+    // linked list!
+    // same idea: [1...n] these numbers can be index in this n+1 vector
+    // it's a linked list, and if there is duplicaiton, there is a circle
+    // to meet the circle, we can use the trick of fast slow pointer
+    //
+    int findDuplicate(vector<int>& nums)
+    {   
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) {
+                break;
             }
         }
-        return 0;//Should not reach here 
+        // index: 0 1 2 3 4
+        // nums:  2 1 3 2 2
+        // 1      sf
+        // 2          s f 
+        // 3            sf --> meet
+        // but the meet point is not neccesarly the duplicated number
+        // look at this cyrcle: 
+        // index: 0 1 2 3 4 5
+        // nums:  1 2 3 4 5 2
+        // becasue 1 is not in the circle, or it could be something else before going cycle, 
+        // the fast & slow is doing: fast eventrually will do one more rand in the circle, but 
+        // they doesn't tell you where we start the circle, which is the number duplicated
+        // if we look at how many nodes they go over:
+        // beforeDup + meetPosIncycle                = SlowDistance = t*1
+        // beforeDup + meetPosIncycle + n*nodesInCycle = FastDistance = t*2
+
+        // so we have beforeDup = n*nodesInCycle - meetPosIncycle
+        // so when this time we put another pointer slow0 = 0 , and start move by one, 
+        // and same time, slow is already at meetPosIncycle, then will do n*nodesInCycle - meetPosIncycle
+        // then slow0 and slow will meet at the duplicate point! 
+
+/*
+Misunderstanding Floyd's Cycle Detection Entry Point
+In the fast and slow pointer approach, the meeting point of the two pointers is not the duplicate number. After they meet, you must start a new pointer from index 0 and move both pointers one step at a time until they meet again. This second meeting point is the cycle entry, which corresponds to the duplicate value.*/
+        int slow2 = 0;
+        while (true) {
+            slow = nums[slow];
+            slow2 = nums[slow2];
+            if (slow == slow2) {
+                return slow;
+            }
+        }
+    
+    
+    
     }
+
+
+
 };
