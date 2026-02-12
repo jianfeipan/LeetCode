e52eeee4d7d1a85f41268ce656508023fa8b80fe
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-02-12 07:35:16 +0100

    Refactor kthSmallest to use _leftFirst helper

--- graph/tree/BST/230. Kth Smallest Element in a BST.cpp
+++ graph/tree/BST/230. Kth Smallest Element in a BST.cpp
@@ -9,28 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
+    //  l->m->right order  = Inorder Traversal : recursion
+    // TIme O(n) space O(n)
+    int _leftFirst(TreeNode* root, int & count, const int k)
+    {
+        if(!root) return -1;
+
+        const auto left = _leftFirst(root->left, count, k);
+        if(left!=-1) return left;
+
+        ++count; if(count == k) return root->val;
+    
+        const auto right = _leftFirst(root->right, count, k);
+        if(right!=-1) return right;
+        
+        return -1;//not yet
+    }
+
     int kthSmallest(TreeNode* root, int k) 
     {
-        return inorderVisit(root, k)->val;    
-    }
-    
-private:
-    TreeNode * inorderVisit(TreeNode * root, int &k)
-    {
-        TreeNode * kth = nullptr;
-        if(root)
-        {
-            kth = inorderVisit(root->left, k);
-
-            if(k>0)
-            {
-                --k;
-                if(k==0) kth = root;
-                else kth = inorderVisit(root->right, k);
-            }
-        }
-        return kth;
+        int count = 0;
+        return _leftFirst(root, count, k);
     }
 };
