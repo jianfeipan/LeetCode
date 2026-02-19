2bafb6c6c3c232cd76482689abf281bdf11dab60
Jianfei PAN <jianfei.pan.tb@gmail.com>
2026-02-19 20:41:36 +0100

    Update leet 200. Number of Islands.cpp

--- graph/leet 200. Number of Islands.cpp
+++ graph/leet 200. Number of Islands.cpp
@@ -1,60 +1,33 @@
 class Solution {
-    /*
-    
-    4 or 8 direction adjacent ? 4    
-    
-    */
-    
-    
-public:
-    
-    int numIslands(vector<vector<char>>& grid) 
-    {
-        return numIslands_DFS(grid);
-    }
-    
+
+/*
+BFS/DFS to paint all connected 1s, then go to next unvisited 1 do the same 
+*/
+
 private:
-    int numIslands_DFS(vector<vector<char>> grid) 
-    {    
-        int islandCount = 0;
-        
-        for(size_t lin = 0; lin<grid.size(); ++lin)
-        {
-            for(size_t col = 0; col<grid[lin].size(); ++col)
-            {
-                if(grid[lin][col] == '1')
-                {
-                    ++islandCount;  
-                    visit(lin, col, grid);
+    void _dfs_visit_island(vector<vector<char>>& grid, int r, int c){
+        if(r<0 || r>=grid.size() || c<0 || c>=grid[r].size()) return;
+
+        if(grid[r][c]!='1') return;
+
+        grid[r][c] = '.';
+        _dfs_visit_island(grid, r+1, c);
+        _dfs_visit_island(grid, r-1, c);
+        _dfs_visit_island(grid, r, c+1);
+        _dfs_visit_island(grid, r, c-1);
+    }
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int num = 0;
+        for(int r=0; r<grid.size(); ++r){
+            for(int c=0;c<grid[r].size(); ++c){
+                if(grid[r][c]=='1'){
+                    ++num;
+                    _dfs_visit_island(grid, r, c);
                 }
             }
         }
-        return islandCount;
-    }
-    
-    void visit(int lin, int col, vector<vector<char>> & grid)
-    {
-        if(lin >=0 && lin<grid.size() && col>=0 && col<grid[lin].size())
-        {
-            if(grid[lin][col] == '1')
-            {
-                grid[lin][col] = 'X';//mark as visited
-                const vector<pair<int, int>> & moves = moveInDirections();
-                
-                for(const auto & move : moves)
-                    visit(lin + move.first, col + move.second , grid);
-            }
-        }
-    }
-    
-    const static vector<pair<int, int>> moveInDirections()
-    {
-        static vector<pair<int, int>> directions({
-            {-1, 0},
-            { 1, 0},
-            { 0,-1},
-            { 0, 1}
-        });
-        return directions;
+        return num;
     }
 };
